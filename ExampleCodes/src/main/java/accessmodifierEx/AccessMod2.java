eef1230b1277539d80cc532ba855ab5ec3bb9d18
hp <hp@LAPTOP-116NDJE9>
2024-04-22 07:39:20 +0530

    AccessModifier

---
+++ ExampleCodes/src/main/java/accessmodifierEx/AccessMod2.java
@@ -0,0 +1,15 @@
+package accessmodifierEx;
+
+import accessmodifier.AccessMod;
+
+public class AccessMod2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AccessMod obj=new AccessMod();
+		obj.meth1();
+		//obj.meth2();protected
+		//obj.meth4();default
+	}
+
+}
