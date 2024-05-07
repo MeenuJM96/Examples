e5e7f1ab66ab1d6165d54676b3df080090a4469a
hp <hp@LAPTOP-116NDJE9>
2024-05-07 21:55:19 +0530

    for each loop

---
+++ ExampleCodes/src/main/java/foreach/ForEachLoop.java
@@ -0,0 +1,13 @@
+package foreach;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String colours[]= {"Red","Orange","Blue"};
+		for(String i:colours) {
+		System.out.println(i);
+		}
+	}
+
+}
